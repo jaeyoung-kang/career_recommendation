a54724f285307039b0aca55d66b3babd3433c87a
LEEMINJOO <minjoo.lee@makinarocks.ai>
2020-12-02 22:42:23 +0900

    UPDATE: career

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import re
+import datetime as dt
 
 
 def strip(series):
@@ -42,21 +43,145 @@ def basic(data):
     return data
 
 
-def career(data):
-    def get_career_time(items):
-        if isinstance(items, float):
-            return []
-        career_list = []
-        for i, item in enumerate(items):
-            if re.search('[0-9]{4}년 [0-9]{1,2}월', item):
-                career_list += [(items[i-1], item)]
-        return career_list
+def career(df):
+    df = df.drop(['school','project','language','award','certificate'],axis=1) # 다른 전처리요소는 제거하기
+    data = pd.DataFrame()
+    for k in range(len(df['career'])):
+        cr = list(map(lambda x : x.replace(' ',''), df['career'][k].split('\n'))) 
+        crd= []
+        for i in range(2,len(cr)):
+            if len(cr[i]) > 0:
+                crd.append(cr[i])
+        cd = pd.DataFrame(crd,columns=['career'])
+        cdd = cd.loc[cd[cd['career'].str.contains('년','월',na=False)][cd[cd['career'].str.contains('년','월',na=False)].index > 2].index-1]
+        cddd = cd.loc[cd[cd['career'].str.contains('년','월',na=False)][cd[cd['career'].str.contains('년','월',na=False)].index > 2].index]
+        cdddd = cd.loc[cd[cd['career'].str.contains('년','월',na=False)][cd[cd['career'].str.contains('년','월',na=False)].index > 2].index-2] # 직무인덱스
+        unna = pd.DataFrame(np.full((len(cd.loc[cd[cd['career'].str.contains('년','월',na=False)][cd[cd['career'].str.contains('년','월',na=False)].index > 2].index-1]), 1), df['id'][k]),columns=['id'])
+        cd = pd.concat([cdd.reset_index(drop=True),unna,cddd.reset_index(drop=True),cdddd.reset_index(drop=True)],axis=1)
+        data = data.append(cd)
+    data.columns = ['career','ID','time','task']
+ 
 
-    career = data['career']
-    career = career.str.replace('경력', '')
-    career = split(strip(career))
-    career = career.apply(get_career_time)
-    return pd.concat([data['id'], career], axis=1)
+   
+    # 데이터에서 특수문자들 제거하기
+    data =data.reset_index(drop=True)
+    data['career'] = pd.DataFrame(map(lambda x : x.replace('\r',''),data['career']))
+    data['time']=pd.DataFrame(map(lambda x : x.replace('\r',''),data['time']))
+    data['task']=pd.DataFrame(map(lambda x : x.replace('\r',''),data['task']))
+    data['career'] =pd.DataFrame(map(lambda x : x.replace('=-',''),data['career']))
+    # time을 2020년6월-현재	4개월 이렇게 언제부터언제까지 근무했는지랑 얼마나 근무했는지로 나누자
+    data = pd.concat([data,pd.DataFrame(map(lambda x : x.split('|'),data['time']))],axis=1)
+    
+    data_1 = data.copy()
+    data_1 = data_1.drop([2,3,4],axis=1)
+    data_1.columns = ['career', 'ID', 'time_2', 'task','time','period']
+    data_1 = data_1.sort_index(axis=1)
+    
+    # career 에서 len이 지나치게 긴것들을 제거했음 (60을 기준으로) 실제 확인해보니까 4개 빼고 전부다 이상치
+    # career 에서 len이 지나치게 긴것들을 제거했음 (60을 기준으로) 실제 확인해보니까 4개 빼고 전부다 이상치
+    idx = []
+    check =[]
+    for k in range(len(data_1['career'])):
+        if type(data_1['career'][k]) == str:
+            if len(data_1['career'][k]) >60:
+                check.append(k)
+            else:
+                idx.append(k)
+    data_1 = data_1.drop(check)
+    data_1.reset_index(drop=True,inplace=True)
+    
+    # len 18을 기준으로 time 이상치 제거했음 ( 이것또한 check에 담아서 눈으로 확인할수있게 함 )
+    idx = []
+    check= []
+    for k in range(len(data_1['time'])):
+        if type(data_1['time'][k]) == str:
+            if len(data_1['time'][k]) >18:
+                check.append(k)
+            else:
+                idx.append(k)
+    data_1 = data_1.drop(check)
+    data_1.reset_index(drop=True,inplace=True)
+    
+    data_1 = data_1.dropna(subset=['period']) # period에 결측치 있는거 삭제
+    
+
+    # 이제 datetime으로 바꾸기 위해서 '-' 을 기준으로 시작날짜 끝날짜 만들자
+    aa = pd.DataFrame(map(lambda x : x.split('-'),data_1['time']))
+    aa.columns= ['start','end']
+    # datetime 형식에 맞게 replace
+    aa['start'] =pd.DataFrame(map(lambda x: x.replace('년','-'),aa['start']))
+    aa['start'] =pd.DataFrame(map(lambda x: x.replace('월','-01'),aa['start']))
+    
+    # datetime에서  len < 9보다 크면 이상치로 간주했음 ! 근데 없었음 ! 이미 이전에 많이 제거해서 필요없는듯 ~!
+    idx = []
+    check= []
+    for k in range(len(aa['start'])):
+        if type(aa['start'][k]) == str:
+            if len(aa['start'][k]) <9:
+                idx.append(k)
+        else:
+            check.append(k)
+    aa = aa.drop(idx).reset_index(drop=True)
+    
+    # datetime으로 바꿨음 ~!
+    st =pd.DataFrame(map(lambda x : dt.datetime.strptime(x,'%Y-%m-%d'),aa['start']))
+    
+    # 끝나는 날도 똑같이 ~! 현재는 11월 1일로 했는데 수정가능함
+    aa['end'] =pd.DataFrame(map(lambda x: x.replace('년','-'),aa['end']))
+    aa['end'] =pd.DataFrame(map(lambda x: x.replace('월','-01'),aa['end']))
+    aa['end'] =pd.DataFrame(map(lambda x: x.replace('현재','2020-11-01'),aa['end']))
+    
+    ed=pd.DataFrame(map(lambda x : dt.datetime.strptime(x,'%Y-%m-%d'),aa['end']))
+    time =pd.concat([st,ed],axis=1)
+    time.columns = ['start','end']
+    
+    # 그리고 다시 datetime으로 바꾼걸 데이터프레임에 붙여줍니다
+    data_3 =pd.concat([data_1.drop(idx).reset_index(drop=True),time],axis=1)
+    # 이제 필요없게된 time 제거
+    data_3=data_3.drop(['time_2','time'],axis=1)
+     # period를 전부다 개월수로 변환하자
+    bb = data_3[['period','ID']].copy()
+    bb['period']=pd.DataFrame(map(lambda x: x.replace('-',''),bb['period']))
+    bb.columns=['period','ID']
+    # 왜 개월이랑 개월 아닌걸로 나누냐면 그냥 1년 이렇게 되어있는 데이터를 구분하려고 ! 왜 구분하냐면 아래에서 쓸 eval 함수 때문에 ! 개월수가 있는건 *12+ 하고
+    # 없으면 *12 만 함 ! 
+    idx =bb[bb['period'].str.endswith('년')].index
+    idx2 = bb[bb['period'].str.endswith('개월')].index
+    idx =idx.append(idx2)
+    data_3 =data_3.iloc[idx]
+    data_3 =data_3.reset_index(drop=True)
+     # period를 전부다 개월수로 변환하자
+    bb = data_3[['period','ID']].copy()
+    bb['period']=pd.DataFrame(map(lambda x: x.replace('-',''),bb['period']))
+    bb.columns=['period','ID']
+    idx =bb[bb['period'].str.endswith('년')].index
+    
+    for i in range(len(bb['period'])):
+        if i in idx:
+            bb['period'][i] =bb['period'][i].replace('년','*12')
+        else :
+            bb['period'][i] =bb['period'][i].replace('년','*12+')
+    bb['period']=pd.DataFrame(map(lambda x: x.replace('개월',''),bb['period']))
+    bb['period']=pd.DataFrame(map(lambda x: eval(x),bb['period']))
+    data_4 = pd.concat([data_3.reset_index(drop=True),bb.drop(['ID'],axis=1).reset_index(drop=True)],axis=1)
+    
+    # turn(이직횟수) 구하는 과정 ! ID로 인덱싱해서 뽑은다음에 거꾸로 정렬해서 그걸 변수로 뽑기
+    id =data_4['ID'].unique()
+    b= pd.DataFrame(columns =['ID', 'career', 'period', 'task', 'start', 'end', 'period','turn'])
+    for i in id:
+        a = pd.concat([data_4[data_4['ID'] == i].sort_values(by='start',ascending=True).reset_index(drop=True),pd.DataFrame(data_4[data_4['ID'] == i].sort_values(by='start',ascending=True).reset_index(drop=True).index,columns=['turn'])],axis=1)
+        b = b.append(a)
+    # 필요없게된 period2랑 turnover 제거 !
+    data_4 = b.copy()
+    data_4.columns = ['ID', 'career', 'period2', 'task', 'start', 'end', 'period','turn']
+    data_4 = data_4.drop(['period2'],axis=1)
+    data_4 = data_4.reset_index(drop=True)
+    data_4['sum_peri']=0
+    # 누적근무기간 구하기
+    data_4['period'] = pd.to_numeric(data_4['period'])
+    data_4['sum_peri'] = data_4.groupby('ID')['period'].cumsum()
+    data_4.sort_values(by='ID').reset_index(drop=True,inplace=True)
+    return data_4
 
 
 def school(data):
