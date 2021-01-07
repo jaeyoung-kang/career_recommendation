12fd263bce9ecb504f3c30092e2593a4ca642c28
LEEMINJOO <eminj1997@naver.com>
2021-01-07 22:19:36 +0900

    UPDATE: augmentation

--- src/dataset/augmentation.py
+++ src/dataset/augmentation.py
@@ -13,19 +13,21 @@ def make_binary_target(
     data = data.explode(all_target_col)
     data = data.reset_index(drop=True)
 
-    data.loc[data[target_col] == data[all_target_col], 'target'] = 1
-    data.loc[data[target_col] != data[all_target_col], 'target'] = 0
+    data.loc[data[target_col] == data[all_target_col], 'label'] = 1
+    data.loc[data[target_col] != data[all_target_col], 'label'] = 0
     data = data.drop(target_col, axis=1)
     data = data.rename({all_target_col: target_col}, axis=1)
-    data = data.drop_duplicates()
 
-    n_positive = int(data['target'].sum())
-    if positive_ratio > 0:
+    data = data.sort_values('label', ascending=False)
+    data = data.drop_duplicates(
+        subset=set(data.columns) - set(['label']),
+    ).sort_index()
+
+    n_positive = int(data['label'].sum())
+    if positive_ratio > 0: # 0보다 작은 경우 그대로 아웃풋
         n_negative = int(n_positive * (1 - positive_ratio) / positive_ratio)
-    else:
-        n_negative = 0
-    negative_index = np.random.choice(data[data['target'] == 0].index, n_negative)
-    positive_index = data[data['target'] == 1].index
-    data = data.loc[np.concatenate([positive_index, negative_index])]
+        negative_index = np.random.choice(data[data['label'] == 0].index, n_negative)
+        positive_index = data[data['label'] == 1].index
+        data = data.loc[np.concatenate([positive_index, negative_index])]
 
     return data.sort_index().reset_index(drop=True)
